8546d675a2948d08bf9f8777c3bea1bb9b2b9d20
Siyamthanda Labase <labasesiyamthanda0@gmail.com>
2023-10-17 17:34:50 +0200

    bio,
    changes a button to card when clicked , reveals a bio

---
+++ script.js
@@ -0,0 +1,10 @@
+// const toggleButton = document.getElementById("toggleButton");
+// const bioCard = document.getElementById("bioCard");
+
+// toggleButton.addEventListener("click", function() {
+//   if (bioCard.style.display === "none" || bioCard.style.display === "") {
+//     bioCard.style.display = "block";
+//   } else {
+//     bioCard.style.display = "none";
+//   }
+// });
